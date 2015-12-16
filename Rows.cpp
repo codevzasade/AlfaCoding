a8d35444eda6128c60b839790d1b8c3f0d7cecfa
zasada <codevzasade@gmail.com>
2015-12-17 00:08:21 +0300

    ver 0.0.2

--- Rows.cpp
+++ Rows.cpp
@@ -2,22 +2,28 @@
 #include "math.h"
 #include "stdio.h"
 #include "stdlib.h"
+const double eps = 1e-10;
 int main()
 {
-	double x, a, S, y, b;
-	int n;
-	for (x = 0.1; x <= 0.9; x += 0.1)
+	double x, a, S, y, b, s1, n;
+	for (x = 0.14; x <= 0.5; x += 0.1)
 	{
+		b = 1;
 		a = x;
 		S = x;
-		for (n = 1; n <= 35; n++)
+		do
 		{
+			b += 1;
+			for (n = 1; n <= b+1; n++)
+			{
 				a = a*x*x;
-				S = S + a/(2*n+1);
-		}
-		y = log((1+x)/(1-x));
-		printf("x=%3.1f  S=%8.4f  y=%8.4f \n", x, S*2, y);
+				S += a / (2 * n + 1);
+			}
+			s1 = a / (2 * b + 1);
+		} while (s1 > eps);
+		y = log((1 + x) / (1 - x));
+		printf("x=%3.2f  S=%8.5f  y=%8.5f \n", x, S*2, y);
 	};
 	getchar();
 	return 0;
-}
+}
