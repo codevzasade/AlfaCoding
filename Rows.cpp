1b614b887231bb40f9ab150010574cdb151fd1b3
zasada <codevzasade@gmail.com>
2015-12-20 00:33:07 +0300

    123

--- Rows.cpp
+++ Rows.cpp
@@ -5,24 +5,25 @@
 const double eps = 1e-10;
 int main()
 {
-	double x, a, S, y, b, s1, n;
-	for (x = 0.14; x <= 0.5; x += 0.1)
+	double x, a, y, s, p;
+	int b, n;
+	for (x = 0.1; x < 0.9; x += 0.1)
 	{
-		b = 1;
-		a = x;
-		S = x;
+		a = x; 
+		p = x; // 1 elemnet ryada, p- elemt ryada
+		s = x; // 1 elemnet ryada, s - summa ryada
+		n = 1; // n dlya 2 elemta, and poton n++
+		b = 0; // summa kolichesva shagov
 		do
 		{
-			b += 1;
-			for (n = 1; n <= b+1; n++)
-			{
-				a = a*x*x;
-				S += a / (2 * n + 1);
-			}
-			s1 = a / (2 * b + 1);
-		} while (s1 > eps);
+				a = a*x*x; // rekyrentnii chislitel
+				p = a / (2 * n + 1); // elemt
+				s += p; // summa ryada
+				n += 1;
+				b += 1;
+		} while (fabs(p) > eps); // poka elemt |p|>eps
 		y = log((1 + x) / (1 - x));
-		printf("x=%3.2f  S=%8.5f  y=%8.5f \n", x, S*2, y);
+		printf("x=%3.2f  S=%8.5f  y=%8.5f b=%d\n", x, s * 2, y, b);
 	};
 	getchar();
 	return 0;
